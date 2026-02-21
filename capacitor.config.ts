4fb536d8abad9961b4baabc680c39767d0e2aa73
safarizkyfaddillah <148610387+safarizkyfaddillah@users.noreply.github.com>
2026-02-21 11:48:30 +0700

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'random-user-generator',
-  webDir: 'www'
-};
-
-export default config;
